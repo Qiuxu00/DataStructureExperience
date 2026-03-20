2cea1ef64d0e6be5574db5d71853663eb8d96316
Qiuxu00 <192013836@qq.com>
2026-03-21 01:51:02 +0800

    1

---
+++ Luogu/DP/P1439.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> A;
+    vector<int> B;
+    vector<vector<int>> dp(B.size(), vector<int>(A.size()));
+    dp[0][0] = (A[0] == B[0]);
+    for (int i = 0; i < B.size(); i++)
+    {
+    }
+}
