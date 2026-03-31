74c2a72bd0e4d7afcf168043c4956ba4f13d47b8
Qiuxu00 <192013836@qq.com>
2026-03-31 21:21:26 +0800

    gre

--- Luogu/Greedy/P1106.cpp
+++ Luogu/Greedy/P1106.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main()
 {
-    string num = "5007489";
-    int n = 3;
+    // --- 补齐的 Input 部分 ---
+    string num;
+    int n;
+    if (!(cin >> num >> n))
+        return 0;
+    // -----------------------
     int left = num.size() - n;
     if (left == 0)
     {
@@ -13,12 +17,44 @@ int main()
     }
     if (left < 0)
     {
-        cout << "n is too large" << endl;
         return 0;
     }
+    int j = 0;
+    vector<char> result;
     for (int i = 0; i < left; i++)
     {
+        int min_pos = -1;
+        int min_ = __INT_MAX__;
+        for (; j <= num.size() - left + i; j++)
+        {
+            if (num[j] - '0' < min_)
+            {
+                min_ = num[j] - '0';
+                min_pos = j;
+            }
         }
-
+        if (result.empty())
+        {
+            if (num[min_pos] != '0')
+            {
+                result.push_back(num[min_pos]);
+            }
+        }
+        else
+        {
+            result.push_back(num[min_pos]);
+        }
+        j = min_pos + 1;
+    }
+    if (result.empty())
+    {
+        cout << '0' << endl;
+        return 0;
+    }
+    for (auto i : result)
+    {
+        cout << i;
+    }
+    cout << endl;
     return 0;
 }
