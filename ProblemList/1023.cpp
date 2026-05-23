9b5216af5057cf255ea46c8d56484839cee19bf4
Qiuxu00 <192013836@qq.com>
2026-05-23 12:01:17 +0800

    1

--- ProblemList/1023.cpp
+++ ProblemList/1023.cpp
@@ -10,13 +10,56 @@ void solve(vector<int> &res)
     {
         cin >> i;
     }
-    vector<vector<int>> road(E, vector<int>(3));
-    for (auto &v : road)
+    vector<vector<pair<int, int>>> Edge(n + 1, vector<pair<int, int>>());
+    for (int i = 0; i < E; i++)
     {
-        for (int &i : v)
+        int from, to, len;
+        cin >> from >> to >> len;
+        Edge[from].push_back({to, len});
+        Edge[to].push_back({from, len});
+    }
+    // dp[i][j]表示的是在最多能够使用i块钱的情况下，从起点到j国的最短距离
+    vector<vector<int>> dp(M + 1, vector<int>(n + 1, 1e9));
+    for (int i = 0; i <= M; i++)
+    {
+        dp[i][s] = 0;
+    }
+
+    for (int i = 1; i <= M; i++)
+    {
+        for (int j = 1; j <= n; j++)
         {
-            cin >> i;
+            if (j != s)
+            {
+                if (i < B[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else
+                {
+                    for (auto p : Edge[j])
+                    {
+                        if (dp[i - B[j - 1]][p.first] == 1e9)
+                        {
+                            dp[i][j] = min(dp[i][j], (int)1e9);
+                        }
+                        else
+                        {
+                            dp[i][j] = min(dp[i][j], dp[i - B[j - 1]][p.first] + p.second);
+                        }
+                    }
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+                }
+            }
         }
+    }
+    if (dp[M][t] != 1e9)
+    {
+        res.push_back(dp[M][t]);
+    }
+    else
+    {
+        res.push_back(-1);
     }
 }
 
